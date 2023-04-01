6a734058cce5cf3672f07d1baa051485a82428fe
ziomzziom <54291606+ziomzziom@users.noreply.github.com>
2023-04-01 19:49:50 +0200

    added error handler
    the error isnt pretty that much but works as expected

--- GTS.Gqueue/Program.cs
+++ GTS.Gqueue/Program.cs
@@ -1,7 +1,9 @@
 using GTS.Gqueue;
 using GTS.Gqueue.Repositories;
 using GTS.Gqueue.Repositories.Interfaces;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.EntityFrameworkCore;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,11 +17,23 @@ builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 
-using(var scope = app.Services.CreateScope())
+using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationContext>();
     dbContext.Database.Migrate();
 }
+
+//Error handler
+app.UseExceptionHandler(errorApp =>
+{
+    errorApp.Run(async context =>
+    {
+        var exception = context.Features.Get<IExceptionHandlerPathFeature>()?.Error;
+        // Log the exception here
+        await context.Response.WriteAsync("An unexpected error has occurred.");
+    });
+});
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
